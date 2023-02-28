e1ab0efdc1a344edf79adb5a21d782c1a9ae5642
Eduardo Rodriguez <eduardo.roda20@gmail.com>
2023-02-27 20:22:06 -0500

    rutas con Flask

---
+++ SEMANA02/dia1/web.py
@@ -0,0 +1,26 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<center><h1>BIENVENIDO A MI WEB CON FLASK</h1></center>'
+
+@app.route('/saludo')
+def saludo():
+    nombre = request.args.get('nombre','no hay nombre')
+    return '<center>Hola {}</center>'.format(nombre)
+
+@app.route('/suma')
+def suma():
+    n1 = request.args.get('n1','0')
+    n2 = request.args.get('n2','0')
+    resultado = int(n1) + int(n2)
+    return '<h1>la suma de {} + {} es igual a {}</h1>'.format(n1,n2,resultado)
+
+@app.route('/resta/<int:n1>/<int:n2>')
+def resta(n1=0,n2=0):
+    resultado = n1 - n2 
+    return '<h1>La resta de {} - {} es {} </h1>'.format(n1,n2,resultado)
+
+app.run(debug=True)
