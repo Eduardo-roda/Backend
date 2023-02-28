6eb936c6c0e9ddad4a253bf4a7b076e3156f6b6a
Eduardo Rodriguez <eduardo.roda20@gmail.com>
2023-02-27 21:49:11 -0500

    ejercicio tipo de cambio scrapping

---
+++ SEMANA02/dia1/webmonedas.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+from TipoCambioSbs import TipoCambioSbs
+from tabulate import tabulate
+
+app = Flask(__name__)
+
+tipoCambio = TipoCambioSbs()
+listaMonedas = tipoCambio.obtenerTipoCambio()
+
+@app.route('/')
+def index():
+    """httpResponse = "<center><h1>TIPOS DE CAMBIO DEL DÍA</h1></center>"
+
+    columnas = ['moneda','compra','venta']
+    tablaMonedas = [moneda.values() for moneda in listaMonedas]
+    htmlTablaMonedas = tabulate(tablaMonedas,headers=columnas,tablefmt='html')
+    print(htmlTablaMonedas)
+    httpResponse += htmlTablaMonedas
+
+    return httpResponse"""
+
+    return render_template('index.html')
+
+app.run(debug=True)
