7c88a1948a36364a62f72e80c85a2dd24c461704
Eduardo Rodriguez <eduardo.roda20@gmail.com>
2023-02-28 20:17:31 -0500

    repaso dia2 SEMANA02

---
+++ SEMANA02/dia2/repaso/main.py
@@ -0,0 +1,29 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    strNombre = request.args.get('nom','nn')
+    #return '<h1>Hola {}</h1>'.format(strNombre)
+    return f'<h1>Hola {strNombre} </h1>'
+
+@app.route('/saludo')
+def saludo():
+    strNombre= request.args.get('nombre','')
+    return render_template('saludo.html',nombre=strNombre)
+
+@app.route('/peliculas')
+def peliculas():
+    categoria = request.args.get('cat','nn')
+    listaPeliculas = ['Rapidos y Furiosos 9','Lord of the Rings 1', 'Volver al Futuro']
+
+    context = {
+        "categoria":categoria,
+        "peliculas":listaPeliculas
+    }
+
+    #return render_template('peliculas.html',peliculas=listaPeliculas,categoria=categoria)
+    return render_template('peliculas.html',**context)
+
+app.run(debug=True)
