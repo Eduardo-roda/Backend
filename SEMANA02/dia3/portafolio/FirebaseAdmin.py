08f6f7254e9c868ba13630fb6c1aa70f3e91ee4e
Eduardo Rodriguez <eduardo.roda20@gmail.com>
2023-03-03 19:48:32 -0500

    dia4

--- SEMANA02/dia3/portafolio/FirebaseAdmin.py
+++ SEMANA02/dia3/portafolio/FirebaseAdmin.py
@@ -3,12 +3,12 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 class FirebaseAdmin:
-
+    
     def __init__(self):
         cred = credentials.Certificate("token.json")
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
-
+        
     def getCollection(self,collectionName):
         listCollection = []
         collectionValues = self.db.collection(collectionName)
@@ -16,9 +16,9 @@ class FirebaseAdmin:
         for doc in docValues:
             dicCollection = doc.to_dict()
             listCollection.append(dicCollection)
-
+            
         return listCollection
     
-fs = FirebaseAdmin()
-lista = fs.getCollection('proyectos')
-print(lista)
+#fs = FirebaseAdmin()
+#lista = fs.getCollection('proyectos')
+#print(lista)
