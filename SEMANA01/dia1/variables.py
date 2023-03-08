6dc13ea32835d7581df5c36c1fd0821f4ccb100b
Eduardo Rodriguez <eduardo.roda20@gmail.com>
2023-03-07 19:39:35 -0500

    semana03 día1

--- SEMANA01/dia1/variables.py
+++
@@ -1,8 +0,0 @@
-#manejo de variables cn python
-#ENTRADA 
-numero1 = input("Ingrese número 1:")
-numero2 = input("Ingrese número 2:")
-#PROCESO
-resultado = int(numero1) + int(numero2)
-#SALIDA
-print(numero1 + " + " + numero2 + " = " + str(resultado))
